9b8124c6da4269e5f0c8d7136aae5afc6ba86ec6
Tommy McG <thomasjmcgovern98@gmail.com>
2022-03-24 14:15:58 -0400

    More HTML functionality to display todays conditions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,30 @@
 const apiKey = "5ee9e8bf24401b639ee5aedbea5aea23"
 
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city");
+var searchTermEl = document.querySelector("#search-term");
+
+var todayResultEl = document.querySelector("#today-result");
+
+var temperatureEl = document.querySelector("#temperature");
+var humidityEl = document.querySelector("#humidity");
+var windspeedEl =  document.querySelector("#wind-speed");
+var uvindexEl = document.querySelector("#uv-index");
+
+var searchHistoryEl = document.querySelector("#search-history");
+
+var searches = [];
+
+// This code to produce the date that will later be used was created by someone else.
+// I used this code and not another API for the sake of simplicity. The original code was taken from:
+// https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+
 // Use the API to get the coordinates of a location by name 
 // The user will enter this location because they most likely
 // Will not know the exact geographic coordinates of the place they're searching
@@ -10,9 +35,14 @@ var getLocation = function(location) {
         if(response.ok) {
             response.json().then(function(data) {
                 console.log(data);
+                if(data.length === 0) {
+                    alert("Error: Cannot find that location.");
+                    return;
+                }
                 var lat = data[0].lat;
                 var lon = data[0].lon;
-                getWeather(lat, lon);
+                var name = data[0].name;
+                getWeather(lat, lon, name);
             })
         } else {
             alert("Error: Cannot find that location.")
@@ -25,14 +55,32 @@ var getLocation = function(location) {
 
 // After the location is found, we will enter the coordinates to 
 // fetch the weather conditions at that place
-var getWeather = function(lat, lon) {
+var getWeather = function(lat, lon, name) {
     console.log(lat, lon);
 
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
 
     fetch(apiUrl).then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
+                // Display the name, date, icon of conditions, temp
+                // humidity, wind speed, UV index
+                searchTermEl.textContent = name + " (" + today + ")";
+
+                // Update today's information accordingly
+                temperatureEl.textContent = "Temperature: " + data.current.temp + "F";
+                humidityEl.textContent = "Humidity: " + data.current.humidity;
+                windspeedEl.textContent =  "Wind Speed: " + data.current.wind_speed + " MPH";
+                uvindexEl.textContent = "UV Index: " + data.current.uvi;
+                
+                // Add this search to the history
+                searches.unshift(name);
+                // To prevent from an infinitely long history, we limit it to size of 5
+                if(searches.length > 5) {
+                    searches.pop();
+                }
+
+                console.log(searches);
                 console.log(data);
             })
         } else {
@@ -44,4 +92,16 @@ var getWeather = function(lat, lon) {
     });
 }
 
-getLocation("Seattle");
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+
+    if(city) {
+        getLocation(city);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a location to search.");
+    }
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
